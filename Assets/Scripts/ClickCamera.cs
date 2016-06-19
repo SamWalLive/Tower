73633b034f83b649109bdff85b58595925fa72e5
SamWalLive <SamWal@live.co.uk>
2016-06-19 22:53:48 +0100

    Added Alderaan Apts Prototype & Basic Controls
    Added new floor art and added a camera control script for click and drag
    up and down

---
+++ Assets/Scripts/ClickCamera.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class ClickCamera : MonoBehaviour
+{
+    public float dragSpeed = 2;
+    private Vector3 dragOrigin;
+
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            dragOrigin = Input.mousePosition;
+            return;
+        }
+
+        if (!Input.GetMouseButton(0)) return;
+
+        Vector3 pos = Camera.main.ScreenToViewportPoint(Input.mousePosition - dragOrigin);
+        Vector3 move = new Vector3(0f, pos.y * dragSpeed, 0f);
+
+        transform.Translate(move, Space.World);
+    }
+
+
+}
