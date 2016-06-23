e8a083748c7d9ace8352b802beab248a1327daf3
SamWalLive <SamWal@live.co.uk>
2016-06-23 12:11:43 +0100

    Level Prefabs & RNG for Levels
    Added some prefabs for arrival and residential levels and added a
    generator to make a random amount of levels on start

---
+++ Assets/Scripts/LevelStarter.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelStarter : MonoBehaviour {
+
+    public float spacer;
+    public GameObject arrivals;
+    public GameObject[] residential;
+    public GameObject[] imperial;
+    public int minimumNumberOfLevels;
+    public int maximumNumberOfLevels;
+
+
+	void Start () {
+
+        Instantiate(arrivals);
+        int count = 1;
+        int numberOfExtraLevels = Random.Range(minimumNumberOfLevels, maximumNumberOfLevels + 1);
+        for (int i = 0; i < numberOfExtraLevels; i++, count++)
+        {
+            Instantiate(residential[Random.Range(0, residential.Length)], new Vector3(0f, count * spacer), arrivals.transform.rotation);
+        }
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
